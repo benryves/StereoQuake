ef2251b2f339eebdbf82b429738f04573af4f616
Ben Ryves <benryves@benryves.com>
2010-02-06 00:34:25 +0000

    Y offset is now limited to multiples of two.

--- Quake2/quake2-3.21/client/cl_scrn.c
+++ Quake2/quake2-3.21/client/cl_scrn.c
@@ -332,7 +332,7 @@ static void SCR_CalcVrect (void)
 	scr_vrect.height &= ~1;
 
 	scr_vrect.x = (viddef.width - scr_vrect.width)/2;
-	scr_vrect.y = (viddef.height - scr_vrect.height)/2;
+	scr_vrect.y = ((viddef.height - scr_vrect.height)/2) & ~1;
 }
 
 
