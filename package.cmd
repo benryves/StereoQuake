957f4b596245ac391dbc337039ac062711fd0eff
Ben Ryves <benryves@benryves.com>
2013-04-03 17:01:43 +0000

    The packaging script now uses the Quake 2 "C Only" builds rather than the assembly versions.

--- package.cmd
+++ package.cmd
@@ -6,8 +6,8 @@ chdir package-temp
 mkdir Quake
 mkdir Quake2
 copy ..\Quake\Release\*.exe Quake
-copy ..\Quake2\quake2-3.21\release\*.exe Quake2
-copy ..\Quake2\quake2-3.21\release\*.dll Quake2
+copy "..\Quake2\quake2-3.21\Release (C Only)\*.exe" Quake2
+copy "..\Quake2\quake2-3.21\Release (C Only)\*.dll" Quake2
 cd Quake2
 mkdir baseq2
 move gamex86.dll baseq2
